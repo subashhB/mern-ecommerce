6ac85897a653fc32d3f85bed9eba5873d35c1481
Admin <subashbaidya9@gmail.com>
2023-01-21 10:14:52 +0545

    User Model and User Route

---
+++ routes/userRoute.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+const User = require('../models/userModel');
+
+router.post('/register', (req,res)=>{
+
+});
